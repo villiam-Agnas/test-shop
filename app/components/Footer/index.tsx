9e577e617b4b28973696c5edae264aaab4130826
besethda <sdarmistead@gmail.com>
2026-08-10 11:38:02 +0000

    added footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,3 +1,7 @@
 export const Footer = () => {
-
+  return (
+    <div className="h-18 w-full justify-center items-center flex bg-gray-800">
+      <h2 className="w-fit text-xl text-white">&copy; Test Shop</h2>
+    </div>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { Footer } from "./components/Footer";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="w-full bg-black">
+      <Footer/>
     </div>
   );
 }
