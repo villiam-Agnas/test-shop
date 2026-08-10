d243d5524d9303cc8ffe1e89ded81cb1656231d1
Villiam Agnas <villiamagnas@gmail.com>
2026-08-10 14:00:25 +0200

    added header and data file

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,3 +1,11 @@
 export const Header = () => {
+  return (
+    <div className="text-center bg-gray-800 p-4">
+      <h1 className="text-white text-7xl p-4">Moria's Secret</h1>
+      <h2 className="text-white">Digging to deep in style</h2>
+    </div>
+  )
 
 }
+
+export default Header
