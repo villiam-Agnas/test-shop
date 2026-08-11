3efdbac1e6a598f01becc4d4e97bb6885d159e2a
Villiam Agnas <villiamagnas@gmail.com>
2026-08-11 17:15:49 +0200

    added header tests

--- app/components/Header/header.test.tsx
+++ app/components/Header/header.test.tsx
@@ -3,11 +3,20 @@ import Header from '.'
 
 
 describe("test that the header works", () => {
-  
-  test("there is only one h1 on the page", () => {
+
+  test("tests that header has one h1 with specific text", () => {
     render(<Header satchel={[]} />)
 
-    const pageTitle = screen.getByRole("heading", { level: 1})
+    const pageTitle = screen.getByRole("heading", { level: 1, "name": /moria's Secret/i})
+
+    expect(pageTitle).toBeInTheDocument()
+  })
+
+
+  test("tests that header has one h2 with specific text", () => {
+    render(<Header satchel={[]} />)
+
+    const pageTitle = screen.getByRole("heading", { level: 2, "name": /digging to deep in style/i})
 
     expect(pageTitle).toBeInTheDocument()
   })
