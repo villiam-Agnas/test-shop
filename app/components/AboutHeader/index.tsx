76259b0d0631557868a9b7d8c82f2177ef19c0a9
Seth Armistead <sdarmistead@gmail.com>
2026-08-16 21:40:13 +0200

    added text about villiam- hoping its not too offensive

---
+++ app/components/AboutHeader/index.tsx
@@ -0,0 +1,7 @@
+export const AboutHeader = () => {
+  return (
+    <div className="text-center relative bg-gray-800 p-4">
+      <h1 className="text-white text-7xl p-4">About Us</h1>
+    </div>
+  )
+}
